24e953acb8f54bee5102731f9beffbc15a860589
mhaslinsky <mhaslinsky@gmail.com>
2021-12-27 20:15:06 -0500

    work page..done? demo csvs added for licensing page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -9,7 +9,11 @@ function Work() {
       <h1 className={classes.header}>Previous Work</h1>
       <div className={classes.cascadingText}>
         <div className={classes.cascadingText__letter}>
-          <a href='https://wildfireatheart.netlify.app/'>
+          <a
+            href='https://wildfireatheart.netlify.app/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             <div className={classes.tint}>
               <img
                 src={wildfirepic}
@@ -24,9 +28,9 @@ function Work() {
               This app pulls live data from NASA's Natural Event tracker API.
               The map is generated using Mapbox gl and wrapped and managed using
               the React-map-gl library. The events from the API are filtered and
-              represented on the map using a marker drawn from the Iconify
-              framework. The app hides data needed to function but not for
-              public consumption using environment variables.
+              represented on the map using a marker taken from the Iconify
+              framework. The app utilizes environment variables for confidential
+              data and is styled only using CSS Modules.
             </h4>
             <div className={classes.linkSection}>
               <a
@@ -49,7 +53,11 @@ function Work() {
           </div>
         </div>
         <div className={classes.cascadingText__letter}>
-          <a href='https://hookrestaurantpage.netlify.app/'>
+          <a
+            href='https://hookrestaurantpage.netlify.app/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             <div className={classes.tint}>
               <img
                 src={menupic}
@@ -58,26 +66,43 @@ function Work() {
               ></img>
             </div>
           </a>
+
           <div className={classes.cardText}>
             <h2>Restaurant Website</h2>
             <h4>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
+              This is a mockup website of my friends' pizzeria. The cart manages
+              its state globally using React Context, with actions to modify
+              it's state provided via useReducer. The menu contents are fetched
+              from Firebase and the finalized orders are posted back to the same
+              database. Various elements are stylized using the Styled
+              Components library others with utilizing CSS Modules.
             </h4>
-            <a
-              className={classes.link}
-              href='https://hookrestaurantpage.netlify.app/'
-            >
-              {">"}Take a Look
-            </a>
+            <div className={classes.linkSection}>
+              <a
+                className={classes.link}
+                href='https://hookrestaurantpage.netlify.app/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Take a Look
+              </a>
+              <a
+                className={classes.link}
+                href='https://github.com/mhaslinsky/menu-site'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                See the Code
+              </a>
+            </div>
           </div>
         </div>
         <div className={classes.cascadingText__letter}>
-          <a href='https://mhaslinsky.github.io/narrate-license/'>
+          <a
+            href='https://mhaslinsky.github.io/narrate-license/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             <div className={classes.tint}>
               <img
                 src={licensepic}
@@ -89,19 +114,39 @@ function Work() {
           <div className={classes.cardText}>
             <h2>License Tracker</h2>
             <h4>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur.
+              This tool was created for my employer before learning ReactJS.
+              Using Javascript, HTML, and CSS this automates a licensing process
+              for an application we sell and support. Utilizing the Papa Parse
+              library two seperate CSVs are parsed. The second CSV is filtered
+              based on what is left after the first is filtered. The output is
+              then displayed on the page in a sortable table.
             </h4>
-            <a
-              className={classes.link}
-              href='https://mhaslinsky.github.io/narrate-license/'
-            >
-              {">"}Take a Look
-            </a>
+            <div className={classes.linkSection}>
+              <a
+                className={classes.link}
+                href='https://mhaslinsky.github.io/narrate-license/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Take a Look
+              </a>
+              <a
+                className={classes.link}
+                href='https://drive.google.com/file/d/1AMD3GCm3hMJzY-ckXz2dZ5egLS3cq-Ci/view?usp=sharing'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Demo CSVs
+              </a>
+              <a
+                className={classes.link}
+                href='https://github.com/mhaslinsky/forestfiretacker'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                See the Code
+              </a>
+            </div>
           </div>
         </div>
       </div>
