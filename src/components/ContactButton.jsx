e2cbb363922eb96b5ddf8874c16d39c89f780b45
mhaslinsky <mhaslinsky@gmail.com>
2021-12-30 20:11:08 -0500

    build

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -1,34 +1,32 @@
 import classes from "./ContactButton.module.scss";
 import { Icon } from "@iconify/react";
-import { Fragment, useRef } from "react";
+import { useRef } from "react";
 
 function ContactButton() {
   const textRef = useRef();
 
-  const clipboardCopy = () => {
+  function clipboardCopy() {
     navigator.clipboard.writeText("mhaslinsky@gmail.com");
 
     textRef.current.classList.remove(classes.fadeAnim);
     void textRef.current.offsetWidth;
     textRef.current.classList.add(classes.fadeAnim);
-  };
+  }
 
   return (
-    <Fragment>
-      <div className={classes.wrapper}>
-        <div className={classes.floatingText} ref={textRef}>
-          Email Copied to Clipboard!
-        </div>
-        <div className={classes.container}>
-          <div onClick={clipboardCopy} className={classes.icon}>
-            <Icon icon='carbon:email' />
-          </div>
-          <button onClick={clipboardCopy} className={classes.button}>
-            Contact Me!
-          </button>
-        </div>
+    <div className={classes.wrapper}>
+      <div className={classes.floatingText} ref={textRef}>
+        Email Copied to Clipboard!
       </div>
-    </Fragment>
+      <div className={classes.container}>
+        <div onClick={clipboardCopy} className={classes.icon}>
+          <Icon icon='carbon:email' />
+        </div>
+        <button onClick={clipboardCopy} className={classes.button}>
+          Contact Me!
+        </button>
+      </div>
+    </div>
   );
 }
 
